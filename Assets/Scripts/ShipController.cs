4fe37b2823eda54d1464ce088d60fe6d4bbb3360
Vanessa Chantal <vanesita@1208.gmail.com>
2026-04-22 13:17:51 -0600

    22/04 primer commit

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+
+    private Rigidbody m_rigidbody;
+
+    void Awake() {
+        m_rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
