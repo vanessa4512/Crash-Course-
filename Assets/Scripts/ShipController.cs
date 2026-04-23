32b84534d6209af74f7448730c585ff8bb01569a
Vanessa Chantal <vanesita@1208.gmail.com>
2026-04-22 21:51:17 -0600

    arreglo de error

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,15 +4,21 @@ using UnityEngine.InputSystem;
 public class ShipController : MonoBehaviour
 {
     private Rigidbody2D m_rigidbody;
-    private float m_turnInput;
-    private float m_forwardInput;
+    private float       m_turnInput;
+    private float       m_forwardInput;
+
+    [SerializeField]
+    private float m_maxSpeed;
     [SerializeField]
     private float m_turnSpeed;
     [SerializeField]
     private float m_moveSpeed;
-
     [SerializeField]
     private float m_stoppingPower;
+    [SerializeField]
+    private Rect m_bounds;
+
+
 
     void Awake() {
         m_rigidbody = gameObject.GetComponent<Rigidbody2D>();
@@ -42,6 +48,38 @@ public class ShipController : MonoBehaviour
         else if (m_forwardInput < 0)
         {
             m_rigidbody.linearVelocity = Vector2.Lerp(m_rigidbody.linearVelocity,Vector2.zero, m_stoppingPower * Time.deltaTime);
+        }
+
+        if (m_rigidbody.linearVelocity.magnitude > m_maxSpeed)
+        {
+            m_rigidbody.linearVelocity = m_rigidbody.linearVelocity.normalized * m_maxSpeed;
+        }
+
+        if (!m_bounds.Contains(transform.position))
+        {
+            Vector2 position = transform.position;
+
+            if (position.x < m_bounds.xMin)
+            {
+                position.x = m_bounds.xMax;
+            }
+
+            if (position.x > m_bounds.xMax)
+            {
+                position.x = m_bounds.xMin;
+            }
+
+            if (position.y < m_bounds.yMin)
+            {
+                position.y = m_bounds.yMax;
+            }
+
+            if (position.y > m_bounds.yMax)
+            {
+                position.y = m_bounds.yMin;
+            }
+             m_rigidbody.position = position;
+
         }
 
     }
