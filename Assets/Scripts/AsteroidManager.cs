cbeb64b114b7a10e714661659f3407bd2f0e2c2e
Vanessa Chantal <vanesita@1208.gmail.com>
2026-04-28 12:53:06 -0600

    1/3 de video terminado

--- Assets/Scripts/AsteroidManager.cs
+++ Assets/Scripts/AsteroidManager.cs
@@ -1,6 +1,7 @@
 using System;
-using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class AsteroidManager : MonoBehaviour
@@ -19,7 +20,7 @@ public class AsteroidManager : MonoBehaviour
             StartCoroutine(SpawnAsteroid());
         }
 
-        private IEnumerator<> SpawnAsteroid() {
+        private IEnumerator SpawnAsteroid() {
             for (int i = 0; i < m_maximumAsteroids; i++)
             {
                 yield return new WaitForSeconds(0.1f);
--- Assets/Scripts/AsteroiidController.cs
+++ Assets/Scripts/AsteroiidController.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : BoundedEntity
 {
+    [SerializeField]
+    private float m_forcePower;
+    [SerializeField]
+    private float m_angularPower;
 
-    //[SerializeField]
-    //private Rect m_bounds;
-
+    private void Start() {
+        m_rigidbody.AddForce(Random.insideUnitCircle * m_rigidbody.mass * m_forcePower, ForceMode2D.Impulse);
+        m_rigidbody.angularVelocity = Random.Range(-m_angularPower, m_angularPower);
+    }
 }
