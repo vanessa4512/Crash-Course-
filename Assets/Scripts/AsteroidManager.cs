58bac16c7b39b58e6c0d9d2bbdfe7f1f726ef151
Vanessa Chantal <vanesita@1208.gmail.com>
2026-04-28 09:30:52 -0600

    segunda semana primer commit

--- Assets/Scripts/AsteroidManager.cs
+++ Assets/Scripts/AsteroidManager.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEngine;
 using System.Collections.Generic;
+using Random = UnityEngine.Random;
 
 public class AsteroidManager : MonoBehaviour
 {
@@ -8,4 +10,33 @@ public class AsteroidManager : MonoBehaviour
 
     [SerializeField]
     private List<GameObject> m_asteroidsPrefabs;
+
+    [SerializeField]
+    private Rect m_spawnArea;
+
+    private void Start()
+        {
+            StartCoroutine(SpawnAsteroid());
+        }
+
+        private IEnumerator<> SpawnAsteroid() {
+            for (int i = 0; i < m_maximumAsteroids; i++)
+            {
+                yield return new WaitForSeconds(0.1f);
+                SpawnRandomAsteroid();
+            }
+        }
+
+
+    private void SpawnRandomAsteroid()
+    {
+        int        index           = Random.Range(0, m_asteroidsPrefabs.Count);
+        GameObject asteroidToSpawn = Instantiate(m_asteroidsPrefabs[index], transform);
+
+        Vector2 spawnPoint = new Vector2(
+                                         Random.Range(m_spawnArea.xMin, m_spawnArea.xMax),
+                                         Random.Range(m_spawnArea.yMin, m_spawnArea.yMax)
+                                        );
+        asteroidToSpawn.transform.position = spawnPoint;
+    }
 }
